356791ae98d1af520d8bba9a427471a4c62e8560
PittMadison <karimovpg@gmail.com>
2021-05-14 14:16:18 +0300

    navigation button skeleton

--- src/components/navigation-button/navigation-button.js
+++ src/components/navigation-button/navigation-button.js
@@ -1,9 +1,20 @@
 import { memo } from 'react';
+import { NavLink } from 'react-router-dom';
+import {
+    NAVIGATION_BUTTON_CLASSNAMES,
+    NAVIGATION_BUTTON_LINKS_MAP, NAVIGATION_BUTTON_NAMES,
+    NAVIGATION_BUTTON_TYPES
+} from './navigation-button.constants';
+import './navigation-button.scss';
 
-export const NavigationButton = memo(() => {
+export const NavigationButton = memo((type = NAVIGATION_BUTTON_TYPES.photos) => {
     return (
-        <div>
-            navigation button
-        </div>
+        <NavLink
+            to={NAVIGATION_BUTTON_LINKS_MAP[type]}
+            className={NAVIGATION_BUTTON_CLASSNAMES.default}
+            activeClassName={NAVIGATION_BUTTON_CLASSNAMES.active}
+        >
+            { NAVIGATION_BUTTON_NAMES[type]}
+        </NavLink>
     );
 });
