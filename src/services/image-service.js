345766cb5067fc973fc56087f9323bcfbb039293
PittMadison <karimovpg@gmail.com>
2021-05-17 00:37:52 +0300

    img size change

--- src/services/image-service.js
+++ src/services/image-service.js
@@ -4,6 +4,6 @@ const getRandomInt = (max) => {
 
 export const getRandomImageUrl = () => {
     const randomId = getRandomInt(30000)
-   return `https://loremflickr.com/320/240?lock=${randomId}`
+   return `https://loremflickr.com/640/480?lock=${randomId}`
 }
 
